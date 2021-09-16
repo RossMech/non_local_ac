738a9fe4babd9b91b15734ec27b40f6330d71a91
RossMech <r.nizinkoskij@gmail.com>
2021-09-16 16:56:33 +0200

    a MaterialValueKernel is finalized, the compling is successfull. The testing is although needed

--- include/kernels/MaterialValueKernel.h
+++ include/kernels/MaterialValueKernel.h
@@ -13,28 +13,6 @@ protected:
   // The method, which sould overwrite the KernelValue method for calculation in Integration Point
   virtual Real precomputeQpResidual() override;
 
+  // The variable, which holds the value of material in quadrature point
+  const MaterialProperty<Real> & _Mat_value;
 };
-
-
-
-/*
-# pragma once
-
-# include "KernelValue.h"
-
-class MaterialValueKernel : public KernelValue
-{
-  public:
-    static InputParameters validParams();
-
-    MaterialValueKernel(const InputParameters & parameters);
-
-  protected:
-    // The method, which should overwritten in KernelValue method
-    virtual KernelValue precomputeQpResidual() override;
-
-    // The variable, which holds the value of material in quadrature point
-#    const Real _Mat_value;
-
-}
-/**/
--- src/kernels/MaterialValueKernel.C
+++ src/kernels/MaterialValueKernel.C
@@ -6,49 +6,19 @@ InputParameters
 MaterialValueKernel::validParams()
 {
   auto params = KernelValue::validParams();
-  params.addClassDescription("Same as `Diffusion` in terms of physics/residual, but the Jacobian "
-                             "is computed using forward automatic differentiation");
+  params.addClassDescription("The kernel, which uses the material as local value of kernel");
+  params.addRequiredParam<MaterialPropertyName>(
+                              "Mat_name", "Name of material, which is used as a local value of the kernel");
   return params;
 }
 
-MaterialValueKernel::MaterialValueKernel(const InputParameters & parameters) : KernelValue(parameters) {}
+MaterialValueKernel::MaterialValueKernel(const InputParameters & parameters)
+      : KernelValue(parameters),
+        _Mat_value(getMaterialProperty<Real>("Mat_name"))
+      {}
 
 Real
 MaterialValueKernel::precomputeQpResidual()
 {
-  return _u[_qp];
-}
-
-
-
-
-/*
-
-#include "MaterialValueKernel.h"
-
-registerMooseObject("NonLocalAC", MaterialValueKernel);
-
-InputParameters
-
-MaterialValueKernel::validParams()
-{
-  auto params = MaterialValueKernel::validParams();
-  params.addClassDescription("The kernel, which takes the material value in kernel");
-  params.addRequiredParam<MaterialPropertyName>("mat_name","Name of material, which is used in this kernel");
-  return params
-}
-
-MaterialValueKernel::MaterialValueKernel(const InputParameters & parameters)
-    : KernelValue(parameters),
-      _Mat_value(getMaterialProperty<Real>("mat_name")),
-{
-}
-
-RealVectorValue
-MaterialValueKernel::precomputeQpResidual()
-{
   return _Mat_value[_qp];
 }
-
-
-/**/
