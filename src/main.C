28ce007be43936df0c299d96ce348ee6ce703bda
RossMech <r.nizinkoskij@gmail.com>
2024-08-12 16:03:52 +0200

    updated the main.C file to current MOOSE standart

--- src/main.C
+++ src/main.C
@@ -8,29 +8,12 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "NonLocalACTestApp.h"
-#include "MooseInit.h"
-#include "Moose.h"
-#include "MooseApp.h"
-#include "AppFactory.h"
-
-// Create a performance log
-PerfLog Moose::perf_log("NonLocalAC");
+#include "MooseMain.h"
 
 // Begin the main program.
-int
-main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  // Initialize MPI, solvers and MOOSE
-  MooseInit init(argc, argv);
-
-  // Register this application's MooseApp and any it depends on
-  NonLocalACTestApp::registerApps();
-
-  // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("NonLocalACTestApp", argc, argv);
-
-  // Execute the application
-  app->run();
+  Moose::main<NonLocalACTestApp>(argc, argv);
 
   return 0;
 }
