b750ffdb6561b2ad868c1cb1c4cc8ba6b0740178
RossMech <r.nizinkoskij@gmail.com>
2026-06-15 16:25:01 +0200

    created a subroutine to work with a single simulation for getting scattering plots for all timesteps

--- scripts_for_pre_and_post_processing/FFT/fft_transformation.py
+++ scripts_for_pre_and_post_processing/FFT/fft_transformation.py
@@ -5,43 +5,6 @@ from paraview import servermanager
 from vtk.util import numpy_support
 import matplotlib.pyplot as plt
 
-# Initial lines for trying the algorithm on real data (later delete)
-reader = paraview.simple.ExodusIIReader(FileName="simulation_output_1.e-s143")
-reader.UpdatePipeline()
-scene = paraview.simple.GetAnimationScene()
-scene.UpdateAnimationUsingDataTimeSteps()
-scene.GoToFirst()
-
-# Resample to the regular grid
-warpByVector1 = paraview.simple.WarpByVector(registrationName='WarpByVector1', Input=reader)
-# Properties modified on warpByVector1
-warpByVector1.ScaleFactor = -1.0
-resampleToImage1 = paraview.simple.ResampleToImage(registrationName='ResampleToImage1', Input=warpByVector1)
-resampleToImage1.SamplingDimensions = [600, 600, 1]
-
-# Transform data to the numpy array
-data = dsa.WrapDataObject(servermanager.Fetch(resampleToImage1))
-eta  = data.PointData["eta"]  # numpy array, shape (n_points,)
-
-coords = numpy_support.vtk_to_numpy(data.GetPoints().GetData())  # shape (n_points, 3)
-
-coords = coords[:,:-1]
-
-x, y = coords[:,0], coords[:,1]
-
-# Reshaping the eta array into 2Didx = np.lexsort((x, y))  # sort y-first, then x
-idx = np.lexsort((x, y))  # sort y-first, then x
-eta_2d = eta[idx]
-
-# Get unique coordinates to determine grid dimensions
-nx = len(np.unique(x))
-ny = len(np.unique(y))
-
-x_min = np.min(nx)
-x_max = np.max(nx)
-
-eta_2d = eta[idx].reshape(ny, nx)
-
 def modified_fft_transformation2(eta_2d: np.typing.NDArray[np.float64]) -> np.typing.NDArray[np.float64]:
 
     # Discrete FFT transformation
@@ -69,9 +32,68 @@ def modified_fft_transformation2(eta_2d: np.typing.NDArray[np.float64]) -> np.ty
     # Convoluted I_density
     I_convoluted = I_discrete * sincsqr_matrix
 
-    print(sincsqr_matrix.shape)
-
     return I_convoluted, qlad2
+
+def handle_single_simulation(simulation_full_name: str,
+                             resample_number: int):
+    # Read the files
+    reader = paraview.simple.ExodusIIReader(FileName=simulation_full_name)
+    reader.UpdatePipeline()
+    scene = paraview.simple.GetAnimationScene()
+    scene.UpdateAnimationUsingDataTimeSteps()
+    scene.GoToFirst()
+
+    # Undeform the simulation mesh
+    warpByVector1 = paraview.simple.WarpByVector(registrationName='WarpByVector1', Input=reader)
+    # Properties modified on warpByVector1
+    warpByVector1.ScaleFactor = -1.0
+
+    # Resample the mesh onto the regular grid
+    resampleToImage1 = paraview.simple.ResampleToImage(registrationName='ResampleToImage1', Input=warpByVector1)
+    resampleToImage1.SamplingDimensions = [resample_number, resample_number, 1]
+
+    # Get the values of the time steps
+    source = paraview.simple.GetActiveSource()
+    timestep_values = source.TimestepValues
+    
+    # Commands for handling the timesteps
+    render_view = paraview.simple.GetActiveViewOrCreate('RenderView')
+    animation_scene = paraview.simple.GetAnimationScene()
+
+    # Output array
+    output_array = np.zeros((resample_number,resample_number,timestep_values.length))
+
+    # Iteration over timesteps and handling data
+    i = 0
+    for t in timestep_values:
+        # Go to the current time step
+        animation_scene.AnimationTime = t
+
+        # Update the pipeline
+        render_view.Update()
+        
+        # Get the data for field variable
+        data = dsa.WrapDataObject(servermanager.Fetch(resampleToImage1))
+        eta_2d  = data.PointData["eta"]  # numpy array, shape (n_points,)
+
+        # Get the coordinates values for sorting the eta values onto 2D shape
+        coords = numpy_support.vtk_to_numpy(data.GetPoints().GetData())  # shape (n_points, 3)
+        coords = coords[:,:-1]
+        x, y = coords[:,0], coords[:,1]
+
+        # Get unique coordinates to determine grid dimensions
+        idx = np.lexsort((x, y))  # sort y-first, then x
+        eta_2d = eta[idx].reshape(ny, nx)
+
+        # Get the modified FFT
+        fft_output, q = modified_fft_transformation2(eta_2d)
+
+        # Save the output array 
+        output_array[:,:,i] = fft_output
+
+        i = i+1
+
+    return output_array, q, timestep_values
 
 # Plotting
 fig, ax = plt.subplots()
